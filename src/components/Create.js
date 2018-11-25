cfcd7117f58f5774d1f17fdbebb790d21a76a089
Jordan Rand <jorrand@gmail.com>
2018-11-25 08:14:46 -0700

    did it

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Input, TextArea, Form } from 'semantic-ui-react';
+import { Input, TextArea, Form, Segment, Container } from 'semantic-ui-react';
 import styled from 'styled-components';
 import CardForm from './CardForm';
 
@@ -10,68 +10,90 @@ const StyledInput = styled(Input)`
 class Create extends React.Component {
 
   state = {
+    loaded: false,
     title: '',
     numberOfCards: 5,
     cardForms: [],
     cards: [],
   }
 
-  handleChange = (e) => {
-    console.log(e)
-    const { value, name, tabIndex } = e.target
-    console.log(this.state.cards[tabIndex])
-    if (name === 'term' || name === 'definition') {
-      const { key, term, definition } = this.state.cards[tabIndex]
-      [name] = value
+  handleClick = (e) => {
+    var temp = ''
+    if (this.node.contains(e.target)) {
+      console.log(e.target)
+      this.handleChange()
     }
-    this.setState({
-      [name]: value,
-    })
   }
 
-  getCardFormState = (cardFormState) => {
-    console.log(cardFormState)
+  handleChange = (e) => {
+    const { value, name, tabIndex } = e.target
+    if (name === 'term') {
+      const newCards = this.state.cards
+      newCards[tabIndex].term = value
+      this.setState({
+        cards: newCards
+      })
+    } else if (name === 'definition') {
+      const newCards = this.state.cards
+      newCards[tabIndex].definition = value
+      this.setState({
+        cards: newCards
+      })
+    } else {
+      this.setState({
+        [name]: value,
+      })
+    }
+  }
+
+  createCardForms = () => {
+    if (this.state.loaded === false) {
+      this.setState({
+        loaded: true
+      }, () => {
+        const { title, numberOfCards, cardForms, cards } = this.state
+        for (var i = 0; i < numberOfCards; i++) {
+          cards.push({key: i, term: '', definition: ''})
+          cardForms.push(
+            <Form key={i}>
+            <TextArea
+              placeholder="Term"
+              name="term"
+              tabIndex={i}
+              rows={2}
+              autoHeight
+              onChange={this.handleChange}
+            >{this.state.cards[i].term}</TextArea>
+            <TextArea
+              placeholder="Definition"
+              name="definition"
+              tabIndex={i}
+              rows={2}
+              autoHeight
+              onChange={this.handleChange}
+            >{this.state.cards[i].definition}</TextArea>
+            </Form>
+          )
+        }
+      })
+    }
+    
   }
 
   render() {
-    const { title, numberOfCards, cardForms, cards } = this.state
-    for (var i = 0; i < numberOfCards; i++) {
-      cards.push({key: i, term: '', definition: ''})
-      cardForms.push(
-        <Form key={i}>
-        <TextArea
-          placeholder="Term"
-          name="term"
-          tabIndex={i}
-          rows={2}
-          autoHeight
-          value={this.state.cards[i].term}
-          onChange={this.handleChange}
-        />
-        <TextArea
-          placeholder="Definition"
-          name="definition"
-          tabIndex={i}
-          rows={2}
-          autoHeight
-          value={this.state.cards[i].definition}
-          onChange={this.handleChange}
-        />
-        </Form>
-      )
-    }
     return (
-      <>
+      <Container>
+        {this.createCardForms(true)}
         <StyledInput
           placeholder="Title"
           name="title"
-          value={title}
+          value={this.state.title}
           onChange={this.handleChange}
         />
-        { cardForms.map((value, i) => {
-          return(value)
+        { this.state.cardForms.map((value, i) => {
+          return(<Segment>{value}</Segment>)
         })}
-      </>
+      </Container>
     )
   }
 }
